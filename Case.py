70ab3e02713c823b064a06715ee9a41303d6f15d
L40S38 <shiozawa.l.aa@m.titech.ac.jp>
2022-01-28 23:13:43 +0900

    add Case.py

---
+++ Case.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import json
+import pandas as pd
+
+with open('ConanData.json',encoding='utf-8') as f:
+    jsn = json.load(f)
+
+datalist = []
+
+#ここまで雛形
+
+for case in jsn.values():
+    #print(case)
+    number=case['Number']
+    title=case['Title']
+    explain=case['Explain']
+    datalist.append([number,title,explain])
+
+df=pd.DataFrame(datalist,columns=["number","title","explain"])
+df.to_csv('dataset_csv/Number-Title-Explain.csv',encoding='utf-8') 
