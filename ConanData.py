e9abd9203aa0db8ce13925cb1aa42634dc21960a
L40S38 <shiozawa.l.aa@m.titech.ac.jp>
2022-01-28 22:05:54 +0900

    json file completed

--- ConanData.py
+++ ConanData.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
-from func import get_soup
-from func import rename_url
-
-import attr
-import requests 
-import bs4 
-import pandas as pd
+from func import * 
+import json
 import codecs
 
 url = 'https://websunday.net/conandb/comics-list/' 
@@ -16,14 +11,15 @@ soup = get_soup(url)
 
 ul = soup.find("ul",attrs={"class","cover"})
 volumes = ul.find_all("li")
-print(volumes)
+#print(volumes)
+dataset = dict()
 case_number=0
 for volume in volumes:
     p = volume.find("p").get_text()
-    print(p)
+    #print(p)
     url_volume = rename_url(volume.find("a").get("href"))
     text = get_soup(url_volume)
-    print(f'p:{p}')
+    #print(f'p:{p}')
     case_list = text.find_all("tr")
     for case_tr in case_list:
         case = case_tr.find_all("td")
@@ -34,20 +30,26 @@ for volume in volumes:
         print(f'case_number:{case_number}')
         case_detail = get_soup(case_url).find("div",attrs={"class","conanDb-main"})
         #print(f'case_detail:{case_detail}')
-        case_title = case_detail.find("h1").get_text()
-        print(f'case_title:{case_title}')
-        case_files = case_detail.find_all("li")
-        for file in case_files:
-            print(file.get_text())
-        case_mchara = case_detail.find("div",attrs={"class","mchara"}).find_all("li")
-        for chara in case_mchara:
-            print(chara.get_text())
-        case_venue = case_detail.find("div",attrs={"class","venue"}).find_all("p")
-        for venue in case_venue:
-            print(venue.get_text())
-        case_gchara = case_detail.find("div",attrs={"class","gchara"}).find("p").get_text()
-        print(f'case_gchara:{case_gchara}')
-        print(case_gchara.split('／'))
-        case_explain = case_detail.find("div",attrs={"class","naiyo__item"}).find("p").get_text()
-        print(case_explain)
+        case_title = get_title(case_detail)
+        case_files = get_files(case_detail)
+        case_mchara = get_mchara(case_detail)
+        case_venue = get_venue(case_detail)
+        case_gchara = get_gchara(case_detail)
+        case_explain = get_explain(case_detail)
+        
+
+        data = {}
+        data['Number'] = case_number
+        data['Title'] = case_title
+        data['Files'] = case_files
+        data['Main Characters'] = case_mchara
+        data['Place'] = case_venue
+        data['Guest Characters'] = case_gchara
+        data['Explain'] = case_explain
+        dataset[case_number] = data
 #updatedetail = soup.find_all("div",attrs={"class","livedetail"})
+#print(dataset)
+fw = codecs.open('ConanData.json', 'w','utf-8') 
+json.dump(dataset, fw, ensure_ascii=False)
+fw.close()
+print("**output completed**")
--- func.py
+++ func.py
@@ -1,5 +1,6 @@
 import requests 
 import bs4 
+import re
 
 def get_soup(url):
     res = requests.get(url)
@@ -10,3 +11,62 @@ def get_soup(url):
 
 def rename_url(url):
     return 'https://websunday.net/' + url
+"""
+def remove_unneslet(s):
+    replacements = {
+        '\r\n'  :'',
+        ' '     :'',
+        '　'    :''
+    }
+    print('{}'.format(''.join(map(re.escape, replacements.keys()))))
+    return re.sub('{}'.format(''.join(map(re.escape, replacements.keys()))), lambda m: replacements[m.group()], s)
+"""
+def remove_unneslet(s):
+    #print(s)
+    s = s.replace('\n\n','').replace('\r\n','').replace(' ','').replace('\n','').replace('\u7c31','').replace('\U00020bb7','').replace('\u2661','').replace('\U000e0100','')
+    return s
+
+def get_title(case_detail):
+    case_title = case_detail.find("h1").get_text()
+    #print(f'case_title:{case_title}')    
+    case_title = remove_unneslet(case_title)
+    return case_title
+
+def get_files(case_detail):
+    case_files = case_detail.find("div",attrs={"class","file"}).find_all("li")
+    rtu = []
+    for file in case_files:
+        #print(file.get_text())  
+        f=remove_unneslet(file.get_text())
+        file_idx = f.find('「')
+        rtu.append({'File Index':f[:file_idx],'File Title':f[file_idx:]})
+    return rtu 
+
+def get_mchara(case_detail):
+    case_mchara = case_detail.find("div",attrs={"class","mchara"}).find_all("li")
+    rtu = []
+    for chara in case_mchara:  
+        c = remove_unneslet(chara.get_text())
+        #print(list(c))
+        rtu.append(c)  
+    #print(rtu)    
+    return rtu
+
+def get_venue(case_detail):
+    case_venue = case_detail.find("div",attrs={"class","venue"}).find("p").get_text()
+    case_venue = remove_unneslet(case_venue)
+    if '／' in case_venue:
+        case_venue = case_venue.split('／')
+    return case_venue  
+
+def get_gchara(case_detail):
+    case_gchara = case_detail.find("div",attrs={"class","gchara"}).find("p").get_text()
+    case_gchara = remove_unneslet(case_gchara)
+    #print(case_gchara.split('／'))  
+    return case_gchara.split('／')
+
+def get_explain(case_detail):
+    case_explain = case_detail.find("div",attrs={"class","naiyo__item"}).find("p").get_text() 
+    case_explain = remove_unneslet(case_explain)
+    #print(case_explain)
+    return case_explain
