371aac005d0e1f2a24352960a1d671ed279149b4
L40S38 <shiozawa.l.aa@m.titech.ac.jp>
2025-05-12 17:46:52 +0900

    データ解析改善

--- MainCharacter.py
+++ MainCharacter.py
@@ -15,8 +15,10 @@ for case in jsn.values():
     title=case['Title']
     mchara=case["Main Characters"]
     for chara in mchara:
+        if chara == "羽田秀":
+            chara = "羽田秀𠮷"
         number+=1
         datalist.append([number,title,chara])
 
-df=pd.DataFrame(datalist,columns=["number","title","mchara"])
+df=pd.DataFrame(datalist,columns=["number","case","mchara"])
 df.to_csv('dataset_csv/Title-MainCharacter.csv',encoding='utf-8',index=None) 
